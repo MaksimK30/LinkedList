5b5e94dee374652777df923c6b1eabc5b13ef9ce
MaksimK30 <makskuzmih@mail.ru>
2023-09-26 00:14:46 +0300

    Проведена перепроектировка списка

--- LinkedList.h
+++ LinkedList.h
@@ -1,47 +1,40 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-template <class T>
+template <typename T>
 class LinkedList{
 public:
-    //Вставить элемент
-    void Add(){
-
+    LinkedList(){
+        size = 0;
+        header = nullptr;
     }
 
-    //Вставляет элемент после текущего
-    void AddNext(){
-
-    }
-
-    //Получить размер списка
-    int GetSize(){
-
-    }
-
-    //Получить элемент по индексу
-    T GetItem(int index){
-
-    }
-
-    //Удалить элемент по индексу
-    void Remove(int index){
-
-    }
-
-    //Получить и удалить последний элемент
-    T Pop(){
-
-    }
-
-    //Получить и удалить элемент по индексу
-    T Pop(int index){
-
+    //Добавить элемент в конце списка
+    void PushBack(T *value){
+        if(header == nullptr){
+            header = Node(value);
+        }else{
+            Node *currentNode = header;
+            while(currentNode != nullptr){
+                currentNode = currentNode->pNextNode;
+            }
+        }
     }
 
 private:
+    class Node{
+    public:
+        Node(T value, Node *nextNode = nullptr){
+            pNextNode = nextNode;
+        }
+
+        Node *pNextNode;
+        T value;
+    };
+
     int size;
-    LinkedList<T> *nextItem;
+    Node *header;
+
 };
 
 #endif // LINKEDLIST_H
